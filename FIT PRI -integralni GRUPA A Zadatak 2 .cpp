9cb99365e74e0d74c0fb46b27ce964bf9593b07f
Alibegovic Jasim <42142523+ghostl33t@users.noreply.github.com>
2019-09-26 15:52:00 +0200

    Add files via upload

---
+++ FIT PRI -integralni GRUPA A Zadatak 2 .cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+/* Zadatak 2
+Napisati program koji će učitati prirodni broj n<=10, a zatim n cijelih brojeva. 
+Za svaki broj treba ispitati radi li se o broju koji je 'dobar'. 
+Zatim sve dobre brojeve prebaciti u novi niz te ispisati njegove elemente i veličinu. 
+Koristiti zasebne funkcije za unos i provjeru je li broj dobar.
+Napomena:Broj  je „dobar“ ako je svaka njegova cifra veća od zbira cifara koji se nalaze desno od nje. 
+Na primjer, 9620 je „dobar“ jer je 2>0, 6>2+0, 9>6+2+0. Drugi primjeri: 8421, 95210, 732. 621, 852 itd.
+*/
+
+using namespace std;
+
+int input()
+{
+	return 1;
+}
+int check()
+{
+	return 1;
+}
+
+int main()
+{
+	return 0;
+}
