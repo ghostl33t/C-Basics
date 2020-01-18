8fd324968d0a0a73d871ca46c7718cdad0b2a689
Alibegovic Jasim <42142523+ghostl33t@users.noreply.github.com>
2020-01-18 14:06:26 +0100

    Update Boje.cpp

--- Boje.cpp
+++ Boje.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
 /*TEKST: https://i.imgur.com/4vr9fh8.png */
-void spustanje(char matrica[][5], char boja,bool ulaz)
+void spustanje(char matrica[][5], char boja)
 {
     for(int i = 0; i < 5; i++)
     {
         for(int j = 0; j < 5; j++)
         {
-            if(ulaz == true)
-                cin>>matrica[i][j];
             if(matrica[i][j] == boja && i - 1 >= 0)
             {
                 matrica[i][j] = matrica[i-1][j];
@@ -22,9 +20,19 @@ int main()
     char brisanje_boje;
     char matrica[5][5];
     cin>>brisanje_boje;
-    spustanje(matrica,brisanje_boje,true);
-    spustanje(matrica,brisanje_boje,false);
-    cout<<brisanje_boje<<endl;
+    for(int i = 0; i < 5; i++)
+    {
+           for(int j = 0; j < 5; j++)
+           {
+               cin>>matrica[i][j];
+               if(matrica[i][j] == brisanje_boje && i - 1 >= 0)
+               {
+                    matrica[i][j] = matrica[i-1][j];
+                    matrica[i-1][j] = brisanje_boje;
+               }
+           }
+    }
+    spustanje(matrica, brisanje_boje);
     for(int i = 0; i < 5; i++)
     {
         for(int j = 0; j < 5; j++)
